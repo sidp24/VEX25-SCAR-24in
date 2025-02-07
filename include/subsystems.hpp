cc03e9bed69dab2c6ce651b547e498cf5bbbcd00
ridxm <rm1838@rutgers.edu>
2025-02-06 23:08:38 -0500

    latest code

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -1,21 +1,10 @@
 #pragma once
 
 #include "api.h"
-// #include "pros/optical.hpp"
-
 // Your motors, sensors, etc. should go here.  Below are examples
 
 // inline pros::Motor intake(1);
 // inline pros::adi::DigitalIn limit_switch('A');
 
-inline pros::Motor conveyor(19);
-inline pros::Motor intake(6);
-
-// inline pros::MotorGroup redirect({20, -7}, pros::v5::MotorGears::red, pros::v5::MotorUnits::degrees);
-inline pros::Motor redirect1(-7);
-inline pros::Motor redirect2(20);
-
-
-// inline pros::Rotation encoder(16);
-inline pros::Optical color(18);
-
+inline pros::MotorGroup intake({7, -16});
+// inline pros::Optical color(10);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include "main.h"
-#include "autons.hpp"
-#include "subsystems.hpp"
 
 /////
 // For installation, upgrading, documentations, and tutorials, check out our website!
@@ -10,12 +8,12 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {-1, 2, -3, 4, -5},     // Left Chassis Ports (negative port will reverse it!)
-    { 11, -12, 13, -14, 15},  // Right Chassis Ports (negative port will reverse it!)
-    
-    8,      // IMU Port
+    {2, -3, 4, -5, -6},      // Left Chassis Ports (negative port will reverse it!)
+    {11, -12, 13, -14, 15},  // Right Chassis Ports (negative port will reverse it!)
+
+    13,    // IMU Port
     2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
-    600);   // Wheel RPM
+    600);  // Wheel RPM
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -31,8 +29,8 @@ void initialize() {
 
   // Configure your chassis controls
   chassis.opcontrol_curve_buttons_toggle(false);  // Enables modifying the controller curve with buttons on the joysticks
-  chassis.opcontrol_drive_activebrake_set(2);    // Sets the active brake kP. We recommend ~2.  0 will disable.
-  chassis.opcontrol_curve_default_set(2.5, 3.5);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
+  chassis.opcontrol_drive_activebrake_set(2);     // Sets the active brake kP. We recommend ~2.  0 will disable.
+  chassis.opcontrol_curve_default_set(2.5, 3.5);  // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp!
   default_constants();
@@ -44,13 +42,13 @@ void initialize() {
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.autons_add({
       Auton("Example Drive\n\nDrive forward and come back.", drive_example),
-    //   Auton("Example Turn\n\nTurn 3 times.", turn_example),
-    //   Auton("Drive and Turn\n\nDrive forward, turn, come back. ", drive_and_turn),
-    //   Auton("Drive and Turn\n\nSlow down during drive.", wait_until_change_speed),
-    //   Auton("Swing Example\n\nSwing in an 'S' curve", swing_example),
-    //   Auton("Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining),
-    //   Auton("Combine all 3 movements", combining_movements),
-    //   Auton("Interference\n\nAfter driving forward, robot performs differently if interfered or not.", interfered_example),
+      Auton("Example Turn\n\nTurn 3 times.", turn_example),
+      Auton("Drive and Turn\n\nDrive forward, turn, come back. ", drive_and_turn),
+      Auton("Drive and Turn\n\nSlow down during drive.", wait_until_change_speed),
+      Auton("Swing Example\n\nSwing in an 'S' curve", swing_example),
+      Auton("Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining),
+      Auton("Combine all 3 movements", combining_movements),
+      Auton("Interference\n\nAfter driving forward, robot performs differently if interfered or not.", interfered_example),
   });
 
   // Initialize chassis and auton selector
@@ -65,8 +63,6 @@ void initialize() {
  * the robot is enabled, this task will exit.
  */
 void disabled() {
-
-   ez::Piston redirectFlip('D', false);
   // . . .
 }
 
@@ -117,143 +113,67 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-  // This is preference to what you like to drive on
-  pros::motor_brake_mode_e_t driver_preference_brake = MOTOR_BRAKE_BRAKE;
+  pros::motor_brake_mode_e_t driver_preference_brake = MOTOR_BRAKE_HOLD;
 
-    chassis.drive_brake_set(driver_preference_brake);
-    redirect1.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
-    redirect2.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  chassis.drive_brake_set(driver_preference_brake);
 
-    intake.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
-    conveyor.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+  ez::Piston clamp('A', false);
+  ez::Piston flipper('G', false);
+  // ez::Piston rush('C', false);
+  ez::Piston leftDoinker('D', false);
+  ez::Piston rightDoinker('H', false);
+  pros::MotorGroup ladyBrown({17, -10});
 
+  intake.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
 
-    //for some reason it doesn't like using the ez namespace in subsystems.hpp
-    ez::Piston clamp('B', true);
-    ez::Piston redirectFlip('D', false);
-
-    bool flipState = false;
-    bool clampState = true;
-
-    bool color_sensing = false;
-    bool redirect_sensing = false;
-
-    int operating_speed = 127;
-
+  bool clampDeployed = false;
+  bool flipperDeployed = false;
+  bool leftDoinkerDeployed = false;
+  bool rightDoinkerDeployed = false;
 
   while (true) {
-
     // chassis.opcontrol_tank();  // Tank control
-    chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
+    chassis.opcontrol_arcade_standard(ez::SPLIT);  // Standard split arcade
     // chassis.opcontrol_arcade_standard(ez::SINGLE);  // Standard single arcade
     // chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
     // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
 
-
-    //Intake + Conveyor
-    if(master.get_digital(DIGITAL_R1)){
-        intake.move(127);
-        conveyor.move(127);
-    }
-    else if(master.get_digital(DIGITAL_R2)){
-        intake.move(-127);
-        conveyor.move(-127);
-    }
-    else{
-        intake.brake();
-        conveyor.brake();
+    if (master.get_digital(DIGITAL_R1)) {
+      intake.move(127);
+    } else if (master.get_digital(DIGITAL_R2)) {
+      intake.move(-127);
+    } else {
+      intake.brake();
     }
 
-    //Redirect
-    if(master.get_digital(DIGITAL_UP)){
-        redirect1.move(127);
-        redirect2.move(127);
-    }
-    else if(master.get_digital(DIGITAL_DOWN)){
-        redirect1.move(-127);
-        redirect2.move(-127);
-    }
-    else{
-        redirect1.brake();
-        redirect2.brake();
+    if (master.get_digital_new_press(DIGITAL_B)) {
+      clampDeployed = !clampDeployed;
+      clamp.set(clampDeployed);
     }
 
-    //clamp
-  if(master.get_digital_new_press(DIGITAL_X)){
-    clampState = !clampState;
-    clamp.set(clampState);
-  }
-
-  if(master.get_digital_new_press(DIGITAL_L1)){
-    flipState = !flipState;
-    redirectFlip.set(flipState);
-  }
-
-
-  double hue = color.get_hue();
-
-  if(master.get_digital_new_press(DIGITAL_A)){
-    redirect_sensing = !redirect_sensing;
-    if(redirect_sensing) operating_speed = 80;
-    else{
-      operating_speed = 127;
+    if (master.get_digital_new_press(DIGITAL_Y)) {
+      rightDoinkerDeployed = !rightDoinkerDeployed;
+      rightDoinker.set(rightDoinkerDeployed);
     }
 
-  }
-
-  if(master.get_digital_new_press(DIGITAL_Y)){
-    color_sensing = !color_sensing;
-  }
-
-  /*Color Sorting:
-  RED RING HUE: 5-15
-  DEFAULT HUE: 30-50
-  BLUE RING HUE: 160-220
-  */
-
-  // if(color_sensing){
-  //   master.rumble(".");
-  //   color.set_led_pwm(100);
-  //   if(red_side){
-  //     if(hue > 160 && hue < 220){
-  //       pros::delay(300);
-  //       conveyor.brake();
-  //     }
-  //   }
-  //   else{
-  //     if(hue > 0 && hue < 20){
-  //       pros::delay(300);
-  //       conveyor.brake();
-  //     }
-  //   }
-  // }
-  // else{
-  //   color.set_led_pwm(0);
-  // }
-
-
-  if(redirect_sensing){
-    color.set_led_pwm(100);      //turn on sensor LED (values 0-100)
-    if(red_side){                      //for red rings
-      if(hue > 0 && hue < 20){          //hue values for red
-        pros::delay(120); //let the conveyor reach almost to the top
-        conveyor.move(-80);   //reverse the conveyor
-        pros::delay(300); //reverse for 300ms
-        conveyor.brake();              //stop conveyor
-      }
+    if (master.get_digital_new_press(DIGITAL_RIGHT)) {
+      leftDoinkerDeployed = !leftDoinkerDeployed;
+      leftDoinker.set(leftDoinkerDeployed);
     }
-    else{
-      if(hue > 160 && hue < 220){
-        pros::delay(120);
-        conveyor.move(-80);
-        pros::delay(300);
-        conveyor.brake();
+
+    if (master.get_digital_new_press(DIGITAL_DOWN)) {
+      flipperDeployed = !flipperDeployed;
+      flipper.set(flipperDeployed);
     }
+
+    if (master.get_digital(DIGITAL_L1)) {
+      ladyBrown.move(127);
+    } else if (master.get_digital(DIGITAL_L2)) {
+      ladyBrown.brake();
+      ladyBrown.move(-127);
+    } else {
+      ladyBrown.brake();
     }
-  }
-  else{
-    color.set_led_pwm(0);
-  }
 
     pros::delay(ez::util::DELAY_TIME);  // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
   }
